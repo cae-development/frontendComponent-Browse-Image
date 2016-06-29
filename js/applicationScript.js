e039c447aeda2bbf5308076216247d3a7c8cdd68
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-29 19:39:01 +0200

    MicroserviceCall edited by Thomas Winkler

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -82,7 +82,7 @@ var loadImages = function(){
     var images = data.images.map( function(image){ 
         var element = $("<div class='img' ><img src='"+image.url+"'/></div>");  
         element.on("click",function(){  
-            delete
+            delete(image.id);
         }); 
         return element;
     });
