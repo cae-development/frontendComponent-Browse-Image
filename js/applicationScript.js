efe15537a1450d2d289d82e1d58e65cf6c6f7a98
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-29 19:57:48 +0200

    MicroserviceCall edited by Thomas Winkler

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -60,7 +60,7 @@ var deleteImage = function(imgId){
   client.sendRequest("DELETE", "images", image, "application/json", {}, false,
   function(data, type) {
     console.log(data); 
-    
+    loadImages();
   },
   function(error) {
     console.log(error);
