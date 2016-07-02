23770630a216b55fb93e05cd2938294caea298bf
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-07-02 22:50:24 +0200

    Code regeneration/Model synchronization

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -44,6 +44,12 @@ var init = function() {
 
   client = new Las2peerWidgetLibrary("http://localhost:8086", iwcCallback);
 
+  $('#add').on('click', function() {
+    //start parameter initiation
+
+    //end parameter initiation
+    addUrl();
+  })
   $('#Reload').on('click', function() {
     //start parameter initiation
 
