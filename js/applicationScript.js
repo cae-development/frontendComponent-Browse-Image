cf3926b3feb9ffc4d20280e358f59301d991e480
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-07-12 10:34:04 +0200

    MicroserviceCall edited by EndUser Two

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -96,7 +96,7 @@ var loadImages = function(){
     var images = data.images.map( function(image){ 
         var element = $("<div class='img' id='" + image .id + "-image' ><img src='"+image.url+"'/></div>");  
         // add the event to the element  
-    element.on("click",function()
+    element.on("click",function() {
         return element;
     });
     //Also update the html element?
