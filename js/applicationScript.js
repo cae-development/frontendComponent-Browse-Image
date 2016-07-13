e91ab6fe547443e2d477e360ca27f36e4be78e04
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-07-13 09:25:12 +0200

    MicroserviceCall edited by EndUser Two

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -93,7 +93,7 @@ var loadImages = function(){
   function(data, type) {
     var images = data.images.map( function(image){ 
         var element = $("<div class='img' id='" + image .id + "-image' ><img src='"+image.url+"'/></div>");  
-         f
+         eleme
         return element; 
     });
     //Also update the html element?
