e6b1c9730639a6d28d19329751bf8d7063f9238f
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-07-13 12:31:46 +0200

    Code regeneration/Model synchronization

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -115,14 +115,10 @@ var loadImages = function(){
 }
 
 // openImage
-var openImage = function(image){
+var openImage = function(){
 //start variable declaration
 //end variable declaration
 
-
-  var intentData = "initialized"; 
-  intentData = JSON.stringify(image);
-  client.sendIntent("openImage",intentData,true);
 
 //Additional own javascript  
 
