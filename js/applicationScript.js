f4a772e1528b166486d52a7979a96d87d8fb5e2b
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-09-06 12:01:54 +0200

    IWC Call[openImage] edited by Thomas Winkler

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -121,7 +121,6 @@ var openImage = function(image){
  
 
   var intentData = "initialized";
-intentData = JSON.stringify(image);
 client.sendIntent("openImage",intentData,true);
 
 //Additional own javascript  
